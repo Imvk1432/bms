a05e078771afea2c8285f612586fb8317f7ee5ad
Vivek Mali <malivivek1432@gmail.com>
2024-05-22 13:45:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Home from './pages//Home';
+import Suppliers from './pages//Suppliers';
+import Categories from './pages/Categories';
+import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+
+    
+    return (
+        <div id="main-container">
+            <Navbar/>
+
+            <Routes>
+                <Route path = "/" element={<Home/>} />
+                <Route path ="/Categories" element={<Categories />} />
+                <Route path = "/Suppliers" element={<Suppliers />}/> 
+            </Routes>
+
+        </div>
+    );
+}
+
+export default App;
